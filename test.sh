fa2b84465bab2c1964a6faf679d972238b453cc4
Matthew Miller <matthew@mi11er.net>
2018-01-13 19:00:35 -0500

    test

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-git push --follow-tags -q https://$GH_TOKEN@github.com/MatthewMi11er/testrepo HEAD:master  > /dev/null 2>&1
+git push --follow-tags -q -S https://$GH_TOKEN@github.com/MatthewMi11er/testrepo HEAD:master  > /dev/null 2>&1
